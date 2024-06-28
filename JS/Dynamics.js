82d6d0c023840a287a2dc09d08af453c4c7edfbd
Victor Isesele <victorisesele@gmail.com>
2024-06-28 22:04:51 +0100

    Improved Web 4

---
+++ JS/Dynamics.js
@@ -0,0 +1,29 @@
+let v = document.getElementById("aH1");
+let portC = document.getElementById("PageTitle");
+alert ("WELCOME TO MY PORTFOLIO WEBSITE");
+let Portfolio = ["Victor Isesele",
+			"Programmer",
+			"Chemical Engineer"];
+let color = ["cornflowerblue", "red", 
+			"yellow"
+			];
+let arr = ["IMAGES/NNPCI.jpg", "IMAGES/NYSCIV.jpg",
+		"IMAGES/NNPCII.jpg"];		
+ 
+var x = document.getElementById("ImgId");
+var y = setInterval (change, 5000);
+check = 0
+function change (){
+			x.src= arr[check];
+			v.innerHTML=Portfolio[check];
+		    portC.style.backgroundColor=color[check];
+			check += 1;
+			if (check == 3){
+				check = 0;
+		}	
+		}
+	
+
+
+ 
+ 
